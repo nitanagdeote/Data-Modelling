40178faab03d3de2cec68c3389944a43476c847d
nitanagdeote <nitanagdeote1@gmail.com>
2015-08-12 23:37:14 +0530

    math functions

---
+++ sin_cos.R
@@ -0,0 +1,8 @@
+ # Sine and cosine function with r
+
+x <- seq(-1,1, 0.05) # argument of the function
+y <- sin(x*pi) 
+z <- cos(x*pi)
+plot(x,y,col = "red",title("sine and cosine function"))
+lines(x,z, color = "blue")
+
---
+++ sine.R
@@ -0,0 +1,6 @@
+#
+pi <- 3.14
+x <- seq(-1,1,0.08)
+t <- sin(2*pi*x)
+plot(x, t , type = "p")
+
